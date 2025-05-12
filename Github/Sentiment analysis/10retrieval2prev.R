2bc9f91a07b88cb5354300652c6ab6ebc2a9023b
mikkel0902 <74595246+mikkel0902@users.noreply.github.com>
2025-05-13 00:57:37 +0200

    y
    y

---
+++ Github/Sentiment analysis/10retrieval2prev.R
@@ -0,0 +1,39 @@
+library(readr)
+library(dplyr)
+library(edgar)
+
+useragent = "mikkel.rmp@gmail.com"
+
+
+# Load your breach filing dataset
+filing_list <- read_delim("id.csv", delim = ";") %>%
+  mutate(
+    cik = as.character(cik),              # CIK must be string (no padding)
+    filing_year = as.numeric(filing_year)
+  ) %>%
+  filter(!is.na(cik), !is.na(filing_year)) %>%
+  distinct(cik, filing_year)
+
+
+# Loop over each row
+for (i in 1:nrow(filing_list)) {
+  cik <- filing_list$cik[i]
+  year <- filing_list$filing_year[i]
+  
+  message(paste("🔍 Processing CIK:", cik, "Year:", year))
+  
+  # Try to download MD&A and save to file
+  tryCatch({
+    mdna_text <- getFilingsHTML(
+      cik.no = cik,
+      form.type = '10-K',
+      filing.year = year -1,
+      useragent = useragent
+    )
+    
+  }, error = function(e) {
+    message(paste("⚠️ Failed for CIK:", cik, "Year:", year, "Reason:", e$message))
+  })
+}
+
+
